d322caf1c984e62eec26b9d8f3d0630d20059036
Pelle Wessman <pelle@kodfabrik.se>
2025-05-30 16:27:37 +0200

    feat: add `LiteralTypes` type

--- lib/util-types.d.ts
+++ lib/util-types.d.ts
@@ -1,5 +1,6 @@
 export type Equal<A, B> = A extends B ? A : never;
 
+// TODO: Add tests that ensures that this always returns a key from LiteralTypes
 export type LiteralTypeOf<T> =
   T extends string ? 'string' :
   T extends number ? 'number' :
@@ -12,5 +13,19 @@ export type LiteralTypeOf<T> =
   T extends object ? 'object' :
   T extends () => any ? 'function' :
   never;
+
+// TODO: Add tests that ensures that this maps to LiteralTypeOf<>
+export type LiteralTypes = {
+  'string': string,
+  'number': number,
+  'bigint': bigint,
+  'boolean': boolean,
+  'symbol': symbol,
+  'undefined': undefined,
+  'null': null,
+  'array': unknown[],
+  'object': object,
+  'function': () => unknown,
+};
 
 export type MaybePromised<T> = T | Promise<T>;
