556b63022aeb18df1645853717b5186b99ed6ff1
Pelle Wessman <pelle@kodfabrik.se>
2026-02-12 14:11:53 +0100

    feat: add Simplify

--- lib/util-types.d.ts
+++ lib/util-types.d.ts
@@ -1,3 +1,11 @@
 export type Equal<A, B> = A extends B ? A : never;
 
 export type MaybePromised<T> = T | Promise<T>;
+
+/**
+ * Flatten intersection types for better IDE display in hover tooltips.
+ * Transforms an interface into a type to aide with assignability.
+ *
+ * @see https://github.com/sindresorhus/type-fest
+ */
+export type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
