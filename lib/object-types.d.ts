114e46e222ba654d3cbcd1f93bfdd0c3e581f66b
Pelle Wessman <pelle@kodfabrik.se>
2026-02-12 14:10:02 +0100

    feat: add IsEmptyObject

--- lib/object-types.d.ts
+++ lib/object-types.d.ts
@@ -2,4 +2,13 @@ export type ObjectEntries<T extends object> = ObjectEntry<T>[];
 export type ObjectEntry<T extends object> = { [K in keyof T]: [K, T[K]] }[keyof T];
 export type ObjectFromEntries<T extends UnknownObjectEntry[]> = { [K in T[number][0]]: (T[number] & [K, unknown])[1] };
 export type PartialKeys<T, Keys extends keyof T> = Omit<T, Keys> & Partial<Pick<T, Keys>>;
-export type UnknownObjectEntry = [string | number | symbol, unknown] | readonly [string | number | symbol, unknown];
+export type UnknownObjectEntry = [PropertyKey, unknown] | readonly [PropertyKey, unknown];
+
+declare const emptyObjectSymbol: unique symbol;
+type EmptyObject = { [emptyObjectSymbol]?: never };
+/**
+ * IsEmptyObject borrowed from type-fest
+ *
+ * @see https://github.com/sindresorhus/type-fest
+ */
+export type IsEmptyObject<T> = T extends EmptyObject ? true : false;
